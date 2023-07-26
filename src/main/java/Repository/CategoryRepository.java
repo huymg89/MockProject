f2b90569a6ebc1ffbcb90d26f2615545dd858f37
Gia Huy <huymg8900@gmail.com>
2023-07-26 15:37:44 +0700

    Project

---
+++ src/main/java/Repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package Repository;
+
+import Entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/Repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package Repository;
+
+import Entity.Category;
+import Entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategory(Category category, Pageable pageable);
+}
